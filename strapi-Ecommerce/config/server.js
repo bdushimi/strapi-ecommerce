b303e539592669c93decbbec91d9ce548f27c061
bdushimi <lespharaons@hotmail.com>
2021-02-27 01:35:22 +0200

    Project setup

---
+++ strapi-Ecommerce/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ef14536c330c2dfd1f1ee09ff580582f'),
+    },
+  },
+});
---
+++ strapi-Ecommerce/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2feb2689-a0c6-4208-adec-c4ad5bd4d1fa'
+};
